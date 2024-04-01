5349397df4319fdec92d315b38e3924af6de0b1b
Aditya <adityasahoo652@gmail.com>
2024-04-02 00:32:59 +0530

    added some more colors

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,1 +1,11 @@
-export const colors = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+export const colors = [
+  "#fe9b72",
+  "#fec971",
+  "#00d4fe",
+  "#b693fd",
+  "#e4ee91",
+  "#ff69b4",
+  "#7fffd4",
+  "#ff6347",
+  "#9370db",
+];

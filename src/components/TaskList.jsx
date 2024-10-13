6ed813ad0076fc4ea2dfb9f9eef0807c076e0351
Aditya <adityasahoo652@gmail.com>
2024-10-13 20:39:15 +0530

    chore: Refactor TaskList and Tasks components for drag and drop functionality

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,20 +1,36 @@
 import "../styles/TaskList.css";
 import PropTypes from "prop-types";
 import Tasks from "./Tasks";
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
 
 const TaskList = ({ tasks }) => {
+  const containerRef = useRef(null);
+  const dispatch = useDispatch();
+
   if (tasks.length === 0) return <h1>No tasks</h1>;
+
+  const handleDrop = (draggedIndex, droppedIndex) => {
+    const updatedTasks = [...tasks];
+    const draggedTask = updatedTasks[draggedIndex];
+    updatedTasks.splice(draggedIndex, 1);
+    updatedTasks.splice(droppedIndex, 0, draggedTask);
+    dispatch({ type: "GET_TASKS", payload: updatedTasks });
+  };
+
   return (
-    <div className="task-container">
+    <div className="task-container" ref={containerRef}>
       <h1>Tasks</h1>
       <div className="task-box">
         {tasks?.map((task, index) => (
           <Tasks
-            key={index}
+            key={task.id}
+            index={index}
             id={task.id}
             description={task.description}
             color={task.color}
             completed={task.completed}
+            onDrop={handleDrop}
           />
         ))}
       </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,18 +4,44 @@ import deleteSvg from "../assets/delete.svg";
 import { useDispatch } from "react-redux";
 import { markAsCompleted, deleteTask } from "../redux/action";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
-const Tasks = ({ id, description, completed, color }) => {
+const Tasks = ({ id, description, completed, color, onDrop, index }) => {
   const dispatch = useDispatch();
+  const [isDragging, setIsDragging] = useState(false);
+
   const deleteHandler = () => {
     dispatch(deleteTask(id));
     toast.success("Task Deleted");
   };
+
+  const dragStartHandler = (e) => {
+    e.dataTransfer.setData("draggedIndex", index);
+    setIsDragging(true);
+  };
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const dropHandler = (e) => {
+    const draggedIndex = e.dataTransfer.getData("draggedIndex");
+    setIsDragging(false);
+    onDrop(draggedIndex, index);
+  };
+  console.log(isDragging);
   return (
     <div
       className="task"
-      style={{ backgroundColor: color, opacity: completed ? 0.5 : 1 }}
-      key={id}
+      style={{
+        backgroundColor: color,
+        opacity: completed ? 0.5 : 1,
+        cursor: isDragging ? "grabbing" : "grab",
+      }}
+      key={`task-${id}`}
+      draggable
+      onDragStart={dragStartHandler}
+      onDragOver={dragOverHandler}
+      onDrop={dropHandler}
     >
       <p className="description">{description}</p>
       <input
@@ -37,6 +63,8 @@ Tasks.propTypes = {
   description: propTypes.string.isRequired,
   completed: propTypes.bool.isRequired,
   color: propTypes.string.isRequired,
+  onDrop: propTypes.func.isRequired,
+  index: propTypes.number.isRequired,
 };
 
 export default Tasks;
